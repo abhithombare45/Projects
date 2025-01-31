e3b6e36d0381888c6f5bce46c0f3487fbacfad8f
Abhijeet Thombare <abhijeetthombare@Abhijeets-Mac-mini.local>
2025-02-01 01:49:43 +0530

    contribution on feature file, to get smooth data pattern on visualizatoin by removing noice.

--- fitness_tracker/src/features/build_features.py
+++ fitness_tracker/src/features/build_features.py
@@ -13,14 +13,41 @@ df = pd.read_pickle(
     "/Users/abhijeetthombare/ab_lib/Projects/fitness_tracker/data/interim/02_data_outliers_removed_chauvenets.pkl"
 )
 
+predictor_column = list(df.columns[:6])
+
+plt.style.use("fivethirtyeight")
+plt.rcParams["figure.figsize"] = (20, 5)
+plt.rcParams["figure.dpi"] = 100
+plt.rcParams["lines.linewidth"] = 2
+
 # --------------------------------------------------------------
 # Dealing with missing values (imputation)
 # --------------------------------------------------------------
+for col in predictor_column:
+    df[col] = df[col].interpolate()
 
+df.info()
 
 # --------------------------------------------------------------
 # Calculating set duration
 # --------------------------------------------------------------
+
+df[df["set"] == 25]["acc_y"].plot()
+df[df["set"] == 50]["acc_y"].plot()
+
+duration = df[df["set"] == 1].index[-1] - df[df["set"] == 1].index[0]
+
+for s in df["set"].unique():
+    start = df[df["set"] == s].index[0]
+    stop = df[df["set"] == s].index[-1]
+
+    duration = stop - start
+    df.loc[(df["set"] == s), "duration"] = duration.seconds
+
+duratoin_df = df.groupby("category")["duration"].mean()
+
+duratoin_df.iloc[0] / 5
+duratoin_df.iloc[1] / 10
 
 
 # --------------------------------------------------------------
