f81ba0a8277d939af8bcc459de5e0a2f77759bf3
Abhijeet Thombare <abhijeetthombare@Abhijeets-Mac-mini.local>
2025-02-01 06:19:34 +0530

    contribution on feature file, removing noice.

--- fitness_tracker/src/features/build_features.py
+++ fitness_tracker/src/features/build_features.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from DataTransformation import PrincipalComponentAnalysis
+from DataTransformation import LowPassFilter, PrincipalComponentAnalysis
 from TemporalAbstraction import NumericalAbstraction
 
 
@@ -54,20 +54,104 @@ duratoin_df.iloc[1] / 10
 # Butterworth lowpass filter
 # --------------------------------------------------------------
 
+df_lowpass = df.copy()
+LowPass = LowPassFilter()
+
+fs = 1000 / 200
+cutoff = 1.3
+
+df_lowpass = LowPass.low_pass_filter(df_lowpass, "acc_y", fs, cutoff, order=5)
+
+subset = df_lowpass[df_lowpass["set"] == 45]
+print(subset["label"][0])
+
+fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(20, 10))
+ax[0].plot(subset["acc_y"].reset_index(drop=True), label="raw_data")
+ax[1].plot(subset["acc_y_lowpass"].reset_index(drop=True), label="Butterworth Filter")
+ax[0].legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), fancybox=True, shadow=True)
+ax[1].legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), fancybox=True, shadow=True)
+
+
+for col in predictor_column:
+    df_lowpass = LowPass.low_pass_filter(df_lowpass, col, fs, cutoff, order=5)
+    df_lowpass[col] = df_lowpass[col + "_lowpass"]
+    del df_lowpass[col + "_lowpass"]
+
 
 # --------------------------------------------------------------
 # Principal component analysis PCA
 # --------------------------------------------------------------
+df_pca = df_lowpass.copy()
 
+PCA = PrincipalComponentAnalysis()
+
+pc_values = PCA.determine_pc_explained_variance(df_pca, predictor_column)
+
+# The Elbow Technioque Explanation
+
+plt.figure(figsize=(10, 10))
+plt.plot(range(1, len(predictor_column) + 1), pc_values)
+plt.xlabel("principal component number")
+plt.ylabel("explained variance")
+plt.show()
+
+df_pca = PCA.apply_pca(df_pca, predictor_column, 3)
+
+subset = df_pca[df_pca["set"] == 45]
+
+subset[["pca_1", "pca_2", "pca_3"]].plot()
 
 # --------------------------------------------------------------
 # Sum of squares attributes
 # --------------------------------------------------------------
 
+df_squared = df_pca.copy()
+acc_r = df_squared["acc_x"] ** 2 + df_squared["acc_x"] ** 2 + df_squared["acc_x"] ** 2
+gyr_r = df_squared["gyr_x"] ** 2 + df_squared["gyr_x"] ** 2 + df_squared["gyr_x"] ** 2
+
+df_squared["acc_r"] = np.sqrt(acc_r)
+df_squared["gyr_r"] = np.sqrt(gyr_r)
+
+subset = df_squared[df_squared["set"] == 14]
+subset[["acc_r", "gyr_r"]].plot()
+
+df_squared
 
 # --------------------------------------------------------------
 # Temporal abstraction
 # --------------------------------------------------------------
+
+# If we check Class and what veriable we need to pass in to the functions,
+# then we are good to declare veriable and work on creating code
+
+df_temporal = df_squared.copy()
+
+NumAbs = NumericalAbstraction()
+
+predictor_column = predictor_column + ["acc_r", "gyr_r"]
+
+ws = int(1000 / 200)
+
+# for mean and Std deviation
+for col in predictor_column:
+    df_temporal = NumAbs.abstract_numerical(df_temporal, [col], ws, "mean")
+    df_temporal = NumAbs.abstract_numerical(df_temporal, [col], ws, "std")
+
+
+df_temporal_list = []
+for s in df_temporal["set"].unique():
+    subset = df_temporal[df_temporal["set"] == s].copy()
+    for col in predictor_column:
+        subset = NumAbs.abstract_numerical(subset, [col], ws, "mean")
+        subset = NumAbs.abstract_numerical(subset, [col], ws, "std")
+    df_temporal_list.append(subset)
+
+df_temporal = pd.concat(df_temporal_list)
+
+df_temporal.info()
+
+subset[["acc_y", "acc_y_temp_mean_ws_5", "acc_y_temp_std_ws_5"]].plot()
+subset[["gyr_y", "gyr_y_temp_mean_ws_5", "gyr_y_temp_std_ws_5"]].plot()
 
 
 # --------------------------------------------------------------
