f68bca40976c26d21b25a95dbbe03e9531fdaa45
Abhijeet Thombare <abhithombare45@gmail.com>
2025-07-08 01:17:18 +0530

    Deploying BHP-Project onAWS EC2 Instance.

--- BangloreHousePrices/client/app.js
+++ BangloreHousePrices/client/app.js
@@ -27,7 +27,8 @@ function onClickedEstimatePrice() {
   var estPrice = document.getElementById("uiEstimatedPrice");
 
   // var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
-  var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+  // var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+    var url = "http://127.0.0.1:5000/predict_home_price"; // Updated to use Flask dev server
 
   $.post(url, {
       total_sqft: parseFloat(sqft.value),
@@ -44,13 +45,15 @@ function onClickedEstimatePrice() {
 function onPageLoad() {
   console.log( "document loaded" );
   // var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
-  var url = "/api/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+  // var url = "/api/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+  var url = "http://127.0.0.1:5000/get_location_names"; // Updated to use correct URL for Flask dev server
   $.get(url,function(data, status) {
       console.log("got response for get_location_names request");
       if(data) {
           var locations = data.locations;
           var uiLocations = document.getElementById("uiLocations");
           $('#uiLocations').empty();
+          $('#uiLocations').append(new Option("Choose a Location", ""));
           for(var i in locations) {
               var opt = new Option(locations[i]);
               $('#uiLocations').append(opt);
